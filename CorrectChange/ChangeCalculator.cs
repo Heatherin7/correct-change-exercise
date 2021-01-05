e8fb3d9e428ebae7dc325b8285fa3f64835cf3ea
heather dougherty <heatherdougherty1234@me.com>
2021-01-04 23:28:26 -0500

    fixed formatting

--- CorrectChange/ChangeCalculator.cs
+++ CorrectChange/ChangeCalculator.cs
@@ -2,18 +2,18 @@
 
 namespace CorrectChange
 {
-	public class ChangeCalculator
-	{
-		/// <summary>
-		/// Returns a Change object representing the minimum number of coins necessary to
-		/// make the cents parameter.
-		/// </summary>
-		public Change CalculateChange(int cents)
-		{
-			if (cents < 0)
+  public class ChangeCalculator
+  {
+    /// <summary>
+    /// Returns a Change object representing the minimum number of coins necessary to
+    /// make the cents parameter.
+    /// </summary>
+    public Change CalculateChange(int cents)
+    {
+      if (cents < 0)
       {
-				throw new ArgumentOutOfRangeException(nameof(cents), "no negative numbers, you dummy");
-			}
+        throw new ArgumentOutOfRangeException(nameof(cents), "no negative numbers, you dummy");
+      }
 
       var change = new Change
       {
@@ -22,20 +22,20 @@ namespace CorrectChange
 
       if ((cents %= 25) != 0)
       {
-				change.Dimes = cents / 10;
-			}
+        change.Dimes = cents / 10;
+      }
 
-			if ((cents %= 10) != 0)
-			{
-				change.Nickles = cents / 5;
-			}
+      if ((cents %= 10) != 0)
+      {
+        change.Nickles = cents / 5;
+      }
 
-			if ((cents %= 5) != 0)
-			{
-				change.Pennies = cents;
-			}
+      if ((cents %= 5) != 0)
+      {
+        change.Pennies = cents;
+      }
 
-			return change;
-		}
-	}
+      return change;
+    }
+  }
 }
